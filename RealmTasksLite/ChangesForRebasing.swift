d7ecb88c8d7f2e9c5128a0091ccffae4e33be4c3
Caleb Talbot <caleb.talbot@motorolasolutions.com>
2017-01-31 10:25:02 -0600

    Add comments

---
+++ RealmTasksLite/ChangesForRebasing.swift
@@ -0,0 +1,11 @@
+//
+//  ChangesForRebasing.swift
+//  RealmTasksLite
+//
+//  Created by Caleb Talbot on 1/31/17.
+//  Copyright © 2017 Motorola Solutions Inc. All rights reserved.
+//
+
+import Foundation
+
+// Here I will track what happens with the changes. This was made on Develop after the initial commit.
