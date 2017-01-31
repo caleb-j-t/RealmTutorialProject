cf4da268f2604bb1f55df1d36d5a05633799fa29
Caleb Talbot <caleb.talbot@motorolasolutions.com>
2017-01-31 10:21:51 -0600

    Initial commit

---
+++ RealmTasksLite/AppDelegate.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:[UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController(style: .plain))
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
