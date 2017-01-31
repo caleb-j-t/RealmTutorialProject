0f61adb31fd4fa34a76b841cae2127fc74f47981
Caleb Talbot <mailcaleb@gmail.com>
2017-01-31 11:10:37 -0600

    Changes to Rebase file and rebase

--- RealmTasksLite/ChangesForRebasing.swift
+++ RealmTasksLite/ChangesForRebasing.swift
@@ -10,6 +10,15 @@ import Foundation
 
 // Here I will track what happens with the changes. This was made on Develop after the initial commit.
 
+
 // Commit 1 on Develop (made after Commit 2 on Rebase) - before Commit 3 on Rebase
 
 // Commit 2 on Develop (made after Commit 2 on Rebase) - before Commit 3 on Rebase
+
+// Commit 1 on Rebase
+
+// Commit 2 on Rebase
+
+// Commit 3 on Rebase (after Commit 2 on Develop)
+
+// Commit 4 on Rebase (after Commit 2 on Develop)
